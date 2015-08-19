c97303feb3968573f944a852f1b6f6739875e3ea
Mark Silliman <mwsilliman@gmail.com>
2015-08-19 14:28:17 -0700

    Copying learn.turtlebot's template

---
+++ about.md
@@ -0,0 +1,38 @@
+---
+layout: page
+title: About
+permalink: /about/
+answerrostag: "about"
+---
+
+For robots to enter the mainstream, a massive number of developers will be needed. Not just developers with lots of funding and years of training, but students, hobbyists and other casual users working alongside seasoned veterans and academics. Formerly insurmountable barriers to entry (money, education, manufacturing, etc.) are rapidly coming down and if we focus on a few specific goals, this collaborative, crowd-sourced, cross-discipline dream can come true:
+
+- **Minimal Cost**: Many developers will need to purchase their own equipment
+- **Open-Source Software**: For collaborative development and shared knowledge
+- **High-School Level Language**:  Supporting a wide range of developer backgrounds
+- **Example-Based Articles**: Teaching quickly through real world solutions
+
+In this article series we wanted to target these four points and give developers a place to start with robotic development. [Fork the project](https://github.com/markwsilliman/turtlebot-tutorial), distribute it, commit changes, add articles and dream big. All content, videos & images are available for you to use as you see fit.
+
+Simply put... Robots are awesome! None of us can do this alone (or even in isolated clusters). Join together and lets start hacking robots!
+
+Change the world, 
+
+![Signature](/assets/sig.png)
+
+Mark Silliman
+
+[Contact information is available on my GitHub page](https://github.com/markwsilliman)
+
+***Full transparency:** I don’t profit from TurtleBot sales nor did I get paid to create this content.*
+
+## List of Contributors
+
+If you’d like to contribute, email me your github username and feel free to add your name below.
+
+* [Mark Silliman](https://github.com/markwsilliman)
+* [Austin Meyers (AK5A)](https://github.com/ak5a)
+* [Melissa Eaton](https://github.com/MustangSally12)
+* [ClearPath Robotics](https://github.com/clearpathrobotics)
+* [Erico Guizzo](https://twitter.com/ericoguizzo)
+* [Tarik Kelestemur](https://github.com/tkelestemur)
---
+++ js/jquery.answer.ros.org.js
@@ -0,0 +1,109 @@
+$( document ).ready(function() {
+	parseAnswerRosOrg(return_tag_AnswerRosOrg());
+	$("#askyourquestion_footer").click(function() {
+		AskAnswerRosOrg();
+	});
+	
+	$("#viewallquestions_footer").click(function() {
+		ViewAllAnswerRosOrg();
+	});
+});
+
+//ViewAllAnswerRosOrg
+function ViewAllAnswerRosOrg() {
+	var tag = return_tag_AnswerRosOrg();
+	var url = 'http://answers.ros.org/questions/tags:' + encodeURIComponent(tag) + '/';
+	window.open(url, '_blank');	
+}
+//end ViewAllAnswerRosOrg
+
+//AskAnswerRosOrg
+function AskAnswerRosOrg() {
+	var tag = return_tag_AnswerRosOrg();
+	tag = tag + ",turtlebot";
+	var url = 'http://answers.ros.org/questions/ask/?tags=' + encodeURIComponent(tag);
+	window.open(url, '_blank');	
+}
+//end AskAnswerRosOrg
+
+function return_tag_AnswerRosOrg() {
+	if($("#answerrostag_hidden").length <= 0) {
+		return "learn_turtlebot_default"; //safety in case it wasn't set
+	}
+	return "learn_turtlebot_" + $("#answerrostag_hidden").val();
+}
+
+//parseAnswerRosOrg
+//inspired from Andrew's reply on http://stackoverflow.com/questions/226663/parse-rss-with-jquery
+function parseAnswerRosOrg(tag) {
+  url = 'http://answers.ros.org/feeds/rss/?tags=' + encodeURIComponent(tag)
+  try {
+  	$.ajax({
+    	url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&callback=?&num=10&q=' + encodeURIComponent(url),
+    	dataType: 'json',
+    	success: function(data) {
+      		parseAnswerRosOrg_callback(data);
+    	},
+		error: function(data) {
+			
+		}
+  	});
+  }
+  catch (err) {
+	return;
+  }
+}
+//end parseAnswerRosOrg
+
+//parseAnswerRosOrg_stripHTML
+//anti-html injection from 3rd party rss
+function parseAnswerRosOrg_stripHTML(dirtyString,maxlength) {
+	//strip white space
+	dirtyString = $.trim(dirtyString);
+	//max length
+	if(dirtyString.length > maxlength) {
+		dirtyString = dirtyString.substring(0,maxlength - 3) + "...";
+	}
+	
+	//protect against html injection in case ROS isn't clearing
+	try {
+    	var container = document.createElement('div');
+    	container.innerHTML = dirtyString;
+    	return container.textContent || container.innerText;
+	}
+	catch (err) {
+		return "";
+	}
+}
+//end parseAnswerRosOrg_stripHTML
+
+//parseAnswerRosOrg_callback
+function parseAnswerRosOrg_callback(data) {
+	try {
+		if (data.responseData.feed && data.responseData.feed.entries) {
+			var tmp_html = "<ul>";
+			count = 0;
+	      	$.each(data.responseData.feed.entries, function (i, e) {
+				count++;
+				if(count <= 5) { //max # of recent threads to show
+		        	tmp_html = tmp_html.concat("<li>");
+					tmp_html = tmp_html.concat("<a target='_blank' href='" + parseAnswerRosOrg_stripHTML(e.link) + "'>");
+					tmp_html = tmp_html.concat(parseAnswerRosOrg_stripHTML(e.title,100));
+					tmp_html = tmp_html.concat("</a> by ");
+					tmp_html = tmp_html.concat(parseAnswerRosOrg_stripHTML(e.author,30))
+					tmp_html = tmp_html.concat("</li>");
+			    } 
+	     	});
+			tmp_html = tmp_html.concat("</ul>");
+			$("#answerrosorg").html(tmp_html);
+
+			if(count == 0) {
+				$(".answerrosorg_hideifnoquestionsexist").hide();				
+			}
+		}
+    }
+	catch (err) {
+		return;
+	}
+}
+//end parseAnswerRosOrg_callback
